c65088cf010614169fa95ddbdf231ed76d44d997
guigallo <guilherme.gg1@hotmail.com>
2019-02-06 14:09:35 -0200

    Using ENV to db

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const config = require('../config/config');
 const mongoose = require('mongoose');
 mongoose.set('useFindAndModify', false);
 
-const connString = `mongodb://${config.dbAddress}:${config.dbPort}/${config.dbDataBase}`;
+const connString = ENV['MONGODB_URI'] || `mongodb://${config.dbAddress}:${config.dbPort}/${config.dbDataBase}`;
 const connection = mongoose.connect(connString, { useNewUrlParser: true });
 
 module.exports = connection;
